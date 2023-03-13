ebb44bed599e0cda3e3525e8bc9b93d9cd7bbfeb
Guanda0120 <1274805330@qq.com>
2023-03-13 21:25:32 +0800

    Paper_Figure Draw

--- Debug/Chapter_5_Material.py
+++ Debug/Chapter_5_Material.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-wood = np.asarray([0.15, 0.32, 0.36, 0.21, 0.39, 0.27])
-wool = np.asarray([0.10, 0.25, 0.52, 0.48, 0.60, 0.64])
+# wood = np.asarray([0.20, 0.32, 0.36, 0.21, 0.39, 0.27])
+# wool = np.asarray([0.10, 0.25, 0.48, 0.46, 0.60, 0.64])
+wood = np.asarray([0.21, 0.56, 0.71, 0.53, 0.63, 0.73])
+agg = np.asarray([0.19, 0.18, 0.28, 0.25, 0.38, 0.36])
+
 x = np.linspace(1, 6, 6)
 freq = [125, 250, 500, '1k', '2k', '4k']
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 plt.rcParams["axes.unicode_minus"] = False
 plt.xlabel(r'频率（Hz）', fontsize=12)
 plt.ylabel(r'吸声系数$\alpha$（-）', fontsize=12)
-plt.plot(x, wood, '-o', color='firebrick', linewidth=0.7, label=r'木纹穿孔板', markersize=4)
-plt.plot(x, wool, '-d', color='grey', linewidth=0.7, label=r'人造皮软包', markersize=4)
+plt.plot(x, wood, '-o', color='firebrick', linewidth=1, label=r'吸声软包', markersize=4)
+plt.plot(x, agg, '-d', color='grey', linewidth=1, label=r'聚砂喷涂', markersize=4)
+# plt.plot(x, wool, '-d', color='grey', linewidth=0.7, label=r'人造皮软包', markersize=4)
 plt.yticks(fontsize=12)
 plt.xticks(x, freq, fontsize=12)
 plt.grid()
--- FileIO/st_2_rhino.py
+++ FileIO/st_2_rhino.py
@@ -6,7 +6,7 @@ from Acoustic.ImageSourceRay import ImageSourceRay
 from Acoustic.Receiver import Receiver
 
 
-def im2rhino(pickle_dir: str, out_sir: str):
+def st2rhino(pickle_dir: str, out_sir: str):
     """
 
     :param pickle_dir:
@@ -21,50 +21,40 @@ def im2rhino(pickle_dir: str, out_sir: str):
         # Create a Rhino file
         rh_file = rhino3dm.File3dm()
         # Layer setting
-        layer_name = ['1st_order', '2nd_order', '3rd_order', '4th_order', '5th_order']
-        layer_color = [(255, 0, 0, 255), (0, 255, 0, 255), (0, 0, 255, 255), (0, 255, 255, 255), (255, 0, 255, 255)]
+        cur_layer = rhino3dm.Layer()
+        cur_layer.Name = 'Stochastic_Ray'
+        cur_layer.Visible = True
+        cur_layer.Color = (255, 128, 128, 255)
+        rh_file.Layers.Add(cur_layer)
 
-        for idx in range(len(layer_name)):
-            cur_layer = rhino3dm.Layer()
-            cur_layer.Name = layer_name[idx]
-            cur_layer.Visible = True
-            cur_layer.Color = layer_color[idx]
-            rh_file.Layers.Add(cur_layer)
 
         # Get layer index
         layer_index = [each.Index for each in rh_file.Layers]
 
-        # Construct Building Object
-        id_list = []
-        layer_id = []
-
         if isinstance(rec, Receiver):
 
-            for imray in rec.im_ray:
+            for st_ray in rec.stochastic_ray:
                 # Point Container
                 point_container = []
-                src_point = rhino3dm.Point3d(imray.src_loc[0], imray.src_loc[1], imray.src_loc[2])
+                src_point = rhino3dm.Point3d(st_ray.init_location[0], st_ray.init_location[1], st_ray.init_location[2])
                 point_container.append(src_point)
-                for pt in imray.ref_pt:
+                for pt in st_ray.start_point:
                     tmp_pt = rhino3dm.Point3d(pt[0], pt[1], pt[2])
                     point_container.append(tmp_pt)
-
-                rec_point = rhino3dm.Point3d(imray.rec_loc[0], imray.rec_loc[1], imray.rec_loc[2])
-                point_container.append(rec_point)
 
                 polyline_rh = rhino3dm.Polyline(point_container)
                 polycur = rhino3dm.Curve.CreateControlPointCurve(polyline_rh, degree=1)
                 id = rh_file.Objects.AddCurve(polycur)
                 # Add Layer
                 cur_obj = rh_file.Objects.FindId(str(id))
-                cur_obj.Attributes.LayerIndex = layer_index[len(point_container)-3]
+                cur_obj.Attributes.LayerIndex = layer_index[0]
 
             rh_file.Settings.ModelUnitSystem = rhino3dm.UnitSystem.Meters
             rh_file.Write(out_sir, 6)
 
 
 if __name__ == '__main__':
-    pickle_dir = r"D:\\PlanningHall\\rec_5.pkl"
-    rhino_dir = r'C:\Users\12748\Desktop\PlHa_rec5.3dm'
-    im2rhino(pickle_dir, rhino_dir)
+    pickle_dir = r"D:\\PlanningHall\\rec_0.pkl"
+    rhino_dir = r'C:\Users\12748\Desktop\Real_rec0.3dm'
+    st2rhino(pickle_dir, rhino_dir)
 
